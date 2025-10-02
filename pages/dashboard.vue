ca0be5e84397279ffde0e74187ff4ec24bcb16c4
Ian Liao <ian.liao@cellopoint.com>
2025-10-02 17:11:03 +0800

    fix: dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,21 +1,4 @@
 <script setup lang="ts">
-import { useTheme } from 'vuetify'
-
-const notifyStore = useNotifyStore()
-const userStore = useUserStore()
-const theme = useTheme()
-const drawer = ref(false)
-const isDark = computed(() => theme.global.name.value === 'dark')
-
-function toggleTheme() {
-  theme.global.name.value = isDark.value ? 'light' : 'dark'
-}
-
-const logout = () => {
-  userStore.LOGOUT()
-  // 这里可以添加你的登出逻辑
-  notifyStore.SHOW_NOTIFY({ message: '已登出', type: 'error' })
-}
 // 这里可以添加你的逻辑
 definePageMeta({
   middleware: 'auth'
