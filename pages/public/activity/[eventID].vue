e9052bfd88475ccd73853740d7f237b590b97636
Ian Liao <ian.liao@cellopoint.com>
2025-10-20 09:53:45 +0800

    update

--- pages/public/activity/[eventID].vue
+++ pages/public/activity/[eventID].vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { getActivityByIdAPI } from '@/api/activity/info.api'
 import type { Activity } from '@/types/interface/activity.interface'
-import { PhCalendarDots, PhCube, PhMapPinLine } from '@phosphor-icons/vue'
+import { PhCalendarDots, PhCaretDown, PhCube, PhMapPinLine } from '@phosphor-icons/vue'
 import dayjs from 'dayjs'
 
 const route = useRoute()
 const eventID = route.params.eventID as string
 
 const activityInfo: Ref<Activity | null> = ref(null)
-
+const showMoreDescription = ref(false)
 const guests = computed(() => {
   return activityInfo.value?.participants.filter(participant => participant.type === 'guest').reverse() || []
 })
@@ -110,23 +110,27 @@ definePageMeta({
             <div class="tw-flex tw-items-center tw-text-xl tw-font-bold tw-gap-2">
               <PhCube size="24px" />  活動簡介
             </div>
-            <div class="tw-my-3" v-html="activityInfo?.description?.replaceAll('\n', '<br/>')">
+            <div class="tw-my-3 " :class="[showMoreDescription ? 'tw-line-clamp-none' : 'tw-line-clamp-3']" v-html="activityInfo?.description?.replaceAll('\n', '<br/>')">
             </div>
-          </div>
-          <!-- 精選照片 -->
-          <div class="tw-py-10">
-            <div class="tw-flex tw-items-center tw-text-xl tw-font-bold tw-gap-2">
-              <PhCube size="24px" />  精選照片
+            <div class="tw-text-sm cursor-pointer tw-justify-center tw-items-center tw-mx-auto tw-flex tw-gap-2" @click="showMoreDescription = !showMoreDescription">
+              <span>{{ showMoreDescription ? '收起' : '展開更多' }}</span>
+              <PhCaretDown size="14px" class="tw-transition-transform" :class="{ 'tw-rotate-180': showMoreDescription }" />
             </div>
-            <div class="tw-my-3" v-html="activityInfo?.description">
+            <!-- 精選照片 -->
+            <div class="tw-py-10">
+              <div class="tw-flex tw-items-center tw-text-xl tw-font-bold tw-gap-2">
+                <PhCube size="24px" />  精選照片
+              </div>
+              <div class="tw-my-3" v-html="activityInfo?.description">
+              </div>
             </div>
-          </div>
-          <!-- 活動相簿 -->
-          <div class="tw-py-10">
-            <div class="tw-flex tw-items-center tw-text-xl tw-font-bold tw-gap-2">
-              <PhCube size="24px" />  活動相簿
-            </div>
-            <div class="tw-my-3" v-html="activityInfo?.description">
+            <!-- 活動相簿 -->
+            <div class="tw-py-10">
+              <div class="tw-flex tw-items-center tw-text-xl tw-font-bold tw-gap-2">
+                <PhCube size="24px" />  活動相簿
+              </div>
+              <div class="tw-my-3 tw-line-clamp-3" v-html="activityInfo?.description">
+              </div>
             </div>
           </div>
         </div>
@@ -152,25 +156,25 @@ definePageMeta({
             </div>
           </div>
         </div>
-        <div v-if="organizers.length" class="tw-w-full tw-py-10 tw-flex tw-flex-col tw-justify-center tw-px-6">
-          <div class="tw-mx-auto tw-font-bold tw-mb-8">活動單位</div>
-          <div class="tw-flex tw-flex-wrap tw-flex tw-justify-center">
-            <div
-              v-for="organizer in organizers"
-              :key="organizer.id"
-              :class="[organizer.url ? 'tw-cursor-pointer hover:tw-shadow-lg' : '']"
-              class="tw-mb-4 tw-flex tw-items-center tw-p-4 tw-gap-1 tw-bg-white tw-rounded-sm tw-shadow-md tw-w-[300px] tw-mr-6 tw-mb-6"
-              @click="organizer.url ? openLink(organizer.url) : null"
-            >
-              <NuxtImg
-                v-if="organizer.avatarUrl"
-                :src="organizer.avatarUrl"
-                class="tw-w-[120px] tw-h-[60px] tw-object-cover tw-mr-2"
-              />
-              <div class="tw-flex-1">
-                <div class="tw-font-semibold tw-text-md">{{ organizer.title }}</div>
-                <div class="tw-text-xl tw-font-bold tw-mt-1">{{ organizer.name }}</div>
-              </div>
+      </div>
+      <div v-if="organizers.length" class="tw-w-full tw-py-10 tw-flex tw-flex-col tw-justify-center tw-px-6">
+        <div class="tw-mx-auto tw-font-bold tw-mb-8">活動單位</div>
+        <div class="tw-flex tw-flex-wrap tw-flex tw-justify-center">
+          <div
+            v-for="organizer in organizers"
+            :key="organizer.id"
+            :class="[organizer.url ? 'tw-cursor-pointer hover:tw-shadow-lg' : '']"
+            class="tw-mb-4 tw-flex tw-items-center tw-p-4 tw-gap-1 tw-bg-white tw-rounded-sm tw-shadow-md tw-w-[300px] tw-mr-6 tw-mb-6"
+            @click="organizer.url ? openLink(organizer.url) : null"
+          >
+            <NuxtImg
+              v-if="organizer.avatarUrl"
+              :src="organizer.avatarUrl"
+              class="tw-w-[120px] tw-h-[60px] tw-object-cover tw-mr-2"
+            />
+            <div class="tw-flex-1">
+              <div class="tw-font-semibold tw-text-md">{{ organizer.title }}</div>
+              <div class="tw-text-xl tw-font-bold tw-mt-1">{{ organizer.name }}</div>
             </div>
           </div>
         </div>
